868debba0dea406173772e0f639a6b57d6c5b153
JoannaChandler <jhazimih@gmail.com>
2020-12-06 13:30:53 -0500

    Updating todos, adding steps to the generator and styling the steps and instructions

--- 00_homework-3/script.js
+++ 00_homework-3/script.js
@@ -1,10 +1,6 @@
 //TODO:
 // Add some error messaging to #password telling user they must set a length first
-// #passwordOptions should be hidden on page load
-// When generateBtn is clicked it should toggle the visually-hidden class on #passwordOption to reveal it
-// When generateBtn is clicked update #password innerText to say "set a password length to get started"
-// When a user chooses a password length, update #password innerText to a random string of that length
-// When a user selects a criteria, update the generated string to include the criteria and update #password innerText to the new string
+// Step 1, Step 2 to length and criteria
 
 //When a user sets the password length, run a function to generate a random string of letters
 //Check the value of the input
@@ -13,6 +9,7 @@
 //When a user selects one of the criteria checkboxes, run a function to update the randomly generated string with the criteria
 //Check for the :checked value of checkboxes
 
+// When generateBtn is clicked update #password innerText to randomly generated string
 
 
 // Assignment Code
@@ -20,10 +17,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
 
 }
 
